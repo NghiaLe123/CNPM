be715db034a5ca7a015a7472141abaf19a020a33
hieu23997 <hieu23997@gmail.com>
2018-12-15 00:39:24 +0700

    Add files via upload

--- training/Võ Minh Hiếu-51503612/VoMinhHieu_51503612.java
+++ training/Võ Minh Hiếu-51503612/VoMinhHieu_51503612.java
@@ -17,6 +17,37 @@ public class MyBigNumber implements stringSum{
 			max = str1;
 		}
 		
+		int flag = 0;	                              // biến lưu gia tri du
+		for(int i=0; i<max ; i++){
+			int num1 = str1 - i - 1;	              // gia tri cuoi cung cua chuoi 1
+			int num2 = str2 - i - 1;	              // gia tri cuoi cung cua chuoi 2
+			
+			char dig_1 = '0';                         // bien luu ki tu khi xuat ket qua ra
+			char dig_2 = '0';	
+			
+			
+			if(num1 >= 0){
+				dig_1 = s1.charAt(num1);	          // cho phep lay ki tu vi tri num1 ra
+			}
+			
+			if(num2 >= 0){
+				dig_2 = s2.charAt(num2);	          // cho phep lay ki tu vi tri num2 ra
+			}
+			
+			
+			int temp1 = dig_1 - '0';	              // tao bien de luu ket qua dig_1
+			int temp2 = dig_2 - '0';	              // tao bien de luu ket qua dig_2
+			int sumAll = temp1 + temp2 + flag;        // bien tinh Tong gia tri
+			
+			if(num1 >= 0){
+				result = result + (sumAll % 10);
+			}else{
+				result = result + sumAll;
+			}
+			flag = sumAll/10 ;                         // cong so du
+		}
+		return result;
+		
 		
 	}
 }
